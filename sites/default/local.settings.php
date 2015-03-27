b80b8f81e99e8302e09b9d669021e25ac8e4f3d1
Nathan Rzepecki <nathan@lionslair.net.au>
2015-03-27 14:51:03 +0800

    updated drupal core

---
+++ sites/default/local.settings.php
@@ -0,0 +1,21 @@
+<?php
+
+$databases = array(
+  'default' =>
+  array(
+    'default' =>
+    array(
+      'database' => 'drupal7',
+      'username' => 'drupal7',
+      'password' => 'cceGVc6vCVHrRyNn',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+$error_reporting = E_ALL;
+$display_errors = TRUE;
+$display_startup_errors = TRUE;
